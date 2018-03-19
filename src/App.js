8a9ee51bf694408deabcf36c6a6484ee9f0b7427
weihai.tang <twh_eastasia@163.com>
2018-03-19 23:48:41 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import logo from './svg/logo.svg';
 import './App.css';
-import {Button } from 'antd-mobile';
 import { Link } from 'react-router-dom';
 
 class App extends Component {
--- src/components/report.jsx
+++ src/components/report.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import {Button } from 'antd-mobile';
 import { Link } from 'react-router-dom';
 import scoreBg from '../svg/score_bg.svg';
+import './report.css';
 
 // Highcharts
 const ReactHighcharts = require('react-highcharts');
@@ -10,7 +11,6 @@ var HighchartsMore = require('highcharts-more');
 HighchartsMore(ReactHighcharts.Highcharts);
 var HighchartsExporting = require('highcharts-exporting');
 HighchartsExporting(ReactHighcharts.Highcharts);
-var Highlight = require('react-highlight');
 var HighchartsSolidGauge = require('highcharts-solid-gauge');
 HighchartsSolidGauge(ReactHighcharts.Highcharts);
 
@@ -19,11 +19,12 @@ var singleExamData = require('../utils/single_exam_chart_data.js');
 var config = singleExamData.get_single_exam_chart_view_data("exam_data.exam_name", 80);
 
 
+
 class Report extends Component {
   render() {
     return (
       <div>
-        <div id="titleContainer">
+        <div id="titleContainer" style={{textAlign:"center"}}>
           <p>xxx的xxx考试报告</p>
         </div>
         
@@ -32,16 +33,16 @@ class Report extends Component {
         </div>
 
         <div id="scoreContainer2" style={{minWidth: "400px", maxWidth: "600px", height: "400px", margin: "auto"}}>
-          <img src="../svg/score_bg.svg"></img>
+          {/* <img src="../svg/score_bg.svg"></img> */}
         </div>
 
 
-        <div id="examDetailsContainer" style={{margin: 150}}>
+        <div id="examDetailsContainer" style={{margin: 150,textAlign:"center"}}>
           做题情况
           什么类型的题目，对了几道错了几道
         </div>
 
-        <div id="suggestionContainer" style={{margin: 150}}>
+        <div id="suggestionContainer" style={{margin: 150,textAlign:"center"}}>
           建议意见
         </div>
         {/* <Button type="primary" inline size="small">share</Button> */}
