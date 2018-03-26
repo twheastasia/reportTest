364f846fe52a97039b55279d75817d267474fa72
weihai.tang <twh_eastasia@163.com>
2018-03-26 23:10:48 +0800

    add some card in exam page

--- src/components/exam.jsx
+++ src/components/exam.jsx
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
-import myProgressBar from './myProgressBar';
 
 const ExamPage = ({}) => {
   return (
     <div>
       <p>This a exam page.</p>
-      <myProgressBar />
+      <div style={{background:"yellow", height:"100px", padding:"10px"}}>
+        <p><strong>第x题</strong></p>
+        <p>题目内容题目内容题目内容题目内容题目内容题目内容题目内容题目内容题目内容____题目内容</p>
+        <div id="imageContainer">
+          
+        </div>
+      </div>
+
     </div>
   );
 }
